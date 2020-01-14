c00fd774d1d596cffcd09a86729ef635a3cfb4ff
Tamás Németh <tomuwhu@gmail.com>
2020-01-14 22:50:19 +0100

    .

--- docs/.vuepress/components/pszr.vue
+++ docs/.vuepress/components/pszr.vue
@@ -1,18 +1,39 @@
 <template>
     <div>
-        {{ t }}
+        <table>
+            <tr v-for="row in t">
+                <th>"{{row[0]}}"</th>
+                <td>{{row[1]}}</td>
+                <td>{{( 100*row[1]/n ).toFixed()}}%</td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
-var n = 5
+import szoveg from 'raw-loader!../public/emberijogok.txt'
+var map = new Map
+szoveg.toLowerCase().split('').forEach( v => {
+                if ( map.has( v ) ) {
+                    map.set( v, map.get(v)+1)
+                } else {
+                    map.set( v, 1 )
+                }
+            })
+var cucc = Array.from( map )
+//var szoveg="cica"
 export default {
     data() {
         return {
-            t: Array(2*n+1).fill(0).map( () => Array(2*n+1).fill(0)) 
+            t: cucc.sort( (a, b) => b[1]-a[1] ),
+            n: szoveg.length
         }
     }
 }
 </script>>
 
-<>
+<style>
+th {
+    font-family: 'Courier New', Courier, monospace;
+}
+</style>
