d8707a520b2e6c810c6fafa6d0889a1fa3e9fb14
Tamás Németh <tomuwhu@gmail.com>
2020-01-20 20:00:37 +0100

    .

--- docs/.vuepress/components/pszr.vue
+++ docs/.vuepress/components/pszr.vue
@@ -1,53 +1,79 @@
 <template>
     <div>
         <canvas id="myChart"></canvas>
-        <table>
+        <table v-if="show">
             <tr v-for="row in t">
                 <th>"{{row[0]}}"</th>
                 <td>{{row[1]}}</td>
                 <td>{{( 100*row[1]/n ).toFixed(2)}}%</td>
             </tr>
         </table>
+        <div class="x" v-else @click="show=true">Táblázat</div>
     </div>
 </template>
 
 <script>
 import Chart from 'chart.js'
-import szoveg from 'raw-loader!../public/emberijogok.txt'
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-    options: {}
-})
+import szoveg from 'raw-loader!../public/toldi.txt'
 var map = new Map
-szoveg.toLowerCase().split('').forEach( v => {
+var rc = (a) => `rgba(${
+    Math.round(Math.random()*215)
+    }, ${
+    Math.round(Math.random()*215)
+    }, ${
+    Math.round(Math.random()*215)
+    }, ${ a })`
+szoveg  .toLowerCase()
+        .split('')
+        .forEach( v => {
                 if ( map.has( v ) ) {
                     map.set( v, map.get(v)+1)
                 } else {
                     map.set( v, 1 )
                 }
-            })
+        })
 var cucc = Array.from( map )
 export default {
     data() {
         return {
             t: cucc.sort( (a, b) => b[1]-a[1] ),
-            n: szoveg.length
+            n: szoveg.length,
+            show: false
         }
+    },
+    mounted() {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: cucc.slice(1,20).map(v => `“${ v[0] }”`),
+                datasets: [{
+                    label: '% of Letters',
+                    data: cucc.slice(1,20).map(v => Math.round(10000*v[1]/szoveg.length)/100),
+                    backgroundColor: Array(25).fill(0).map( () => rc(0.2)),
+                    borderColor:  rc(1),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        })
     }
 }
 </script>>
 
 <style>
+div.x {
+    cursor: pointer;
+    text-align: center;
+}
 th {
     font-family: 'Courier New', Courier, monospace;
 }
